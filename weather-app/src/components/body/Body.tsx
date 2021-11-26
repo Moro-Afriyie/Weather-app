792d485f45d70dec476d19442e5463bb168ccbe3
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-11-26 16:24:07 -0100

    create the body html template

--- weather-app/src/components/body/Body.tsx
+++ weather-app/src/components/body/Body.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import "./body.scss";
+import rainyIcon from "../../assets/Shower.png";
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface IBodyProps {}
@@ -7,7 +8,60 @@ interface IBodyProps {}
 const Body: React.FunctionComponent<IBodyProps> = (props) => {
   return (
     <div className="body">
-      <h1>Body</h1>
+      <header className="header">
+        <div className="celcius">
+          <span>&#8451;</span>
+        </div>
+        <div className="farenheit">
+          <span>&#8457;</span>
+        </div>
+      </header>
+      <div className="week__weather">
+        {/* {[1, 2, 3, 4, 5].map((card) => {
+          return (
+            <div className="weather__card" key={card}>
+              <p className="day">Sun, 7 Jun</p>
+              <div className="weather__icon">
+                <img src={rainyIcon} alt="rainy" />
+              </div>
+              <div className="temperature">
+                <div className="max">
+                  <p>16</p>
+                  <span>&#8451;</span>
+                </div>
+                <div className="min">
+                  <p>16</p>
+                  <span>&#8451;</span>
+                </div>
+              </div>
+            </div>
+          );
+        })} */}
+        <div className="weather__card">
+          <p className="day">Sun, 7 Jun</p>
+          <div className="weather__icon">
+            <img src={rainyIcon} alt="rainy" />
+          </div>
+          <div className="temperature">
+            <div className="max">
+              <p>16</p>
+              <span>&#8451;</span>
+            </div>
+            <div className="min">
+              <p>16</p>
+              <span>&#8451;</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="weather__highlights">
+        <p>Today&apos;s Highlights</p>
+        <div className="card">
+          <p className="title">Wind Status</p>
+          <p className="quantity">7mph</p>
+          <p className="icon">7mph</p>
+        </div>
+      </div>
     </div>
   );
 };
