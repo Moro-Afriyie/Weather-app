8cf765c6be5d5edbeeabdcdac015b0e85023889e
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-11-26 17:25:16 -0100

    style the cards

--- weather-app/src/components/body/Body.tsx
+++ weather-app/src/components/body/Body.tsx
@@ -8,73 +8,83 @@ interface IBodyProps {}
 const Body: React.FunctionComponent<IBodyProps> = (props) => {
   return (
     <div className="body">
-      <header className="header">
-        <div className="celcius active">
-          <sup>
-            <span>&#176;</span>
-          </sup>
-          <p>C</p>
-        </div>
-        <div className="farenheit">
-          <sup>
-            <span>&#176;</span>
-          </sup>
-          <p>F</p>
-        </div>
-      </header>
-      <div className="week__weather">
-        {/* {[1, 2, 3, 4, 5].map((card) => {
-          return (
-            <div className="weather__card" key={card}>
-              <p className="day">Sun, 7 Jun</p>
-              <div className="weather__icon">
-                <img src={rainyIcon} alt="rainy" />
-              </div>
-              <div className="temperature">
-                <div className="max">
-                  <p>16</p>
-                  <span>&#8451;</span>
+      <div className="container">
+        <header className="header">
+          <div className="celcius active">
+            <sup>
+              <span>&#176;</span>
+            </sup>
+            <p>C</p>
+          </div>
+          <div className="farenheit">
+            <sup>
+              <span>&#176;</span>
+            </sup>
+            <p>F</p>
+          </div>
+        </header>
+        <div className="weekly__weather">
+          {[1, 2, 3, 4, 5].map((card) => {
+            return (
+              <div className="weather__card" key={card}>
+                <p className="day">Sun, 7 Jun</p>
+                <div className="weather__icon">
+                  <img src={rainyIcon} alt="rainy" />
                 </div>
-                <div className="min">
-                  <p>16</p>
-                  <span>&#8451;</span>
+                <div className="temperature">
+                  <div className="max">
+                    <p>16</p>
+                    <sup>
+                      <span>&#176;</span>
+                    </sup>
+                    <p>C</p>
+                  </div>
+                  <div className="min">
+                    <p>11</p>
+                    <sup>
+                      <span>&#176;</span>
+                    </sup>
+                    <p>C</p>
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })} */}
-        <div className="weather__card">
-          <p className="day">Sun, 7 Jun</p>
-          <div className="weather__icon">
-            <img src={rainyIcon} alt="rainy" />
-          </div>
-          <div className="temperature">
-            <div className="max">
-              <p>16</p>
-              <sup>
-                <span>&#176;</span>
-              </sup>
-              <p>C</p>
-            </div>
-            <div className="min">
-              <p>11</p>
-              <sup>
-                <span>&#176;</span>
-              </sup>
-              <p>C</p>
-            </div>
-          </div>
+            );
+          })}
         </div>
-      </div>
-      <div className="weather__highlights">
-        <p className="heading">Today&apos;s Highlights</p>
-        <div className="card">
-          <p className="title">Wind Status</p>
-          <div className="quantity">
-            <p>7</p>
-            <span>mph</span>
+        <div className="weather__highlights">
+          <p className="heading">Today&apos;s Highlights</p>
+          <div className="card__container">
+            <div className="card">
+              <p className="title">Wind Status</p>
+              <div className="quantity">
+                <p>7</p>
+                <span>mph</span>
+              </div>
+              <p className="icon">WSW</p>
+            </div>
+            <div className="card">
+              <p className="title">Wind Status</p>
+              <div className="quantity">
+                <p>7</p>
+                <span>mph</span>
+              </div>
+              <p className="icon">WSW</p>
+            </div>
+            <div className="card2">
+              <p className="title">Wind Status</p>
+              <div className="quantity">
+                <p>7</p>
+                <span>mph</span>
+              </div>
+            </div>
+            <div className="card2">
+              <p className="title">Wind Status</p>
+              <div className="quantity">
+                <p>7</p>
+                <span>mph</span>
+              </div>
+            </div>
           </div>
-          <p className="icon">WSW</p>
         </div>
       </div>
     </div>
