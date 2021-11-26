43c6ca0b2f1d722d79ee841eb959578b4d4ca8a0
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-11-26 11:07:26 -0100

    style the sidebar

--- weather-app/src/components/sidebar/Sidebar.tsx
+++ weather-app/src/components/sidebar/Sidebar.tsx
@@ -8,12 +8,19 @@ const SideBar: React.FunctionComponent<ISideBarProps> = (props) => {
   return (
     <div className="sidebar">
       <div className="sidebar__search">
-        <button className="search__btn">search for places</button>
+        <button className="search__btn">Search for places</button>
         <div className="gps">
           <span className="material-icons">gps_fixed</span>
         </div>
       </div>
-      <div className="sidebar__cloud"></div>
+      <div className="sidebar__details"></div>
+      <div className="sidebar__date">
+        <p>Today</p> . <p>Fri, 5 Jun</p>
+      </div>
+      <div className="sidebar__location">
+        <span className="material-icons">place</span>
+        <p>Helsinki</p>
+      </div>
     </div>
   );
 };
