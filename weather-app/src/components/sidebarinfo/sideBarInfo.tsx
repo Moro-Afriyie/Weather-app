76bbd9fbf130bd3095e714fc8e3e67437bbc5447
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-12-17 16:11:21 -0100

    create sidebarInfo component

---
+++ weather-app/src/components/sidebarinfo/sideBarInfo.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface ISideBarInfoProps {}
+
+const SideBarInfo: React.FunctionComponent<ISideBarInfoProps> = (props) => {
+  return <h1>Moro</h1>;
+};
+
+export default SideBarInfo;
