47880cb59c37a6294178e658833358328fbac8f4
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-12-17 16:17:34 -0100

    style the sidebarInfo

--- weather-app/src/components/sidebar/Sidebar.tsx
+++ weather-app/src/components/sidebar/Sidebar.tsx
@@ -1,11 +1,18 @@
 import * as React from "react";
+import Search from "../search/Search";
+import SideBarInfo from "../sidebarinfo/sideBarInfo";
 import "./sidebar.scss";
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface ISideBarProps {}
 
 const SideBar: React.FunctionComponent<ISideBarProps> = (props) => {
-  return <div className="sidebar"></div>;
+  return (
+    <div className="sidebar">
+      <SideBarInfo />
+      {/* <Search /> */}
+    </div>
+  );
 };
 
 export default SideBar;
---
+++ weather-app/src/components/sidebarinfo/sideBarInfo.tsx
@@ -0,0 +1,41 @@
+import * as React from "react";
+import rainyIcon from "../../assets/Shower.png";
+import "./sideBarInfo.scss";
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface ISideBarInfoProps {}
+
+const SideBarInfo: React.FunctionComponent<ISideBarInfoProps> = (props) => {
+  return (
+    <div className="sidebar__info">
+      <div className="sidebar__search">
+        <button className="search__btn">Search for places</button>
+        <div className="gps">
+          <span className="material-icons">gps_fixed</span>
+        </div>
+      </div>
+      <div className="sidebar__details">
+        <div className="weather__icon">
+          <img src={rainyIcon} alt="rainy" />
+        </div>
+        <div className="weather__temperature">
+          <p className="temperature">15</p>
+          <span className="degreecelcius">&#8451;</span>
+          {/* <span className="degreefarenheit">&#8457;</span> */}
+        </div>
+        <div className="weather__description">
+          <p>Shower</p>
+        </div>
+      </div>
+      <div className="sidebar__date">
+        <p>Today</p> . <p>Fri, 5 Jun</p>
+      </div>
+      <div className="sidebar__location">
+        <span className="material-icons">place</span>
+        <p>Helsinki</p>
+      </div>
+    </div>
+  );
+};
+
+export default SideBarInfo;
