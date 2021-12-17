9b3852892022c1a8cb0ee90bc7c269fc54e00f3b
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-12-17 16:12:50 -0100

    add the jsx for the sidebarInfo

--- weather-app/src/components/sidebar/Sidebar.tsx
+++ weather-app/src/components/sidebar/Sidebar.tsx
@@ -1,41 +1,11 @@
 import * as React from "react";
 import "./sidebar.scss";
-import rainyIcon from "../../assets/Shower.png";
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface ISideBarProps {}
 
 const SideBar: React.FunctionComponent<ISideBarProps> = (props) => {
-  return (
-    <div className="sidebar">
-      <div className="sidebar__search">
-        <button className="search__btn">Search for places</button>
-        <div className="gps">
-          <span className="material-icons">gps_fixed</span>
-        </div>
-      </div>
-      <div className="sidebar__details">
-        <div className="weather__icon">
-          <img src={rainyIcon} alt="rainy" />
-        </div>
-        <div className="weather__temperature">
-          <p className="temperature">15</p>
-          <span className="degreecelcius">&#8451;</span>
-          {/* <span className="degreefarenheit">&#8457;</span> */}
-        </div>
-        <div className="weather__description">
-          <p>Shower</p>
-        </div>
-      </div>
-      <div className="sidebar__date">
-        <p>Today</p> . <p>Fri, 5 Jun</p>
-      </div>
-      <div className="sidebar__location">
-        <span className="material-icons">place</span>
-        <p>Helsinki</p>
-      </div>
-    </div>
-  );
+  return <div className="sidebar"></div>;
 };
 
 export default SideBar;
